386bab72e180b151d23627b4af99f30c1a80c25c
zet777 <rzet777@gmail.com>
2022-01-25 20:13:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint quontiti, price;
+            {
+                Console.Write("Количество плитки: ");
+                quontiti =  Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Цена зф 1м. кв. плитки: ");
+                price = Convert.ToUInt32(Console.ReadLine());
+            }
+
+            decimal cost = quontiti * price; // руб. 
+
+            decimal discount;
+            {
+                const decimal DISCOUNT_PERSENTAGE = 10; // %
+                discount = cost / 100 * DISCOUNT_PERSENTAGE;
+            }
+            
+             decimal paymentAmoount = cost - discount;
+
+            Console.WriteLine($"Общая стоимость плитки: {cost} руб.");
+            Console.WriteLine($"Скидка                : {discount} руб.");
+            Console.WriteLine($"Сумма к оплате        : {paymentAmoount} руб.");
+
+            Console.ReadLine();
+        }
+    }
+}
